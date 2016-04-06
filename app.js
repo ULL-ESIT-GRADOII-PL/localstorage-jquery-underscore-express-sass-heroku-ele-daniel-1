76742d0544c33423d52171c5974fefaeeffb6fd0
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-04-06 18:06:12 +0100

    adding ejs

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 var express = require('express');
 var app = express();
 
-// Set up static server
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  response.render('pages/index');
 });
 
 app.listen(3000, function () {
