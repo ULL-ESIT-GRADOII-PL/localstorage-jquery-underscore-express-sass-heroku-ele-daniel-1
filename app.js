833f8884d265722ed991a19ecbfc3567fe461d6f
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-04-07 17:05:40 +0100

    adding underscore

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 var express = require('express');
 var app = express();
 
+var _ = require("underscore");
+
+app.locals._ = _;
+
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
