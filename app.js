4bc8265aed6619f832ec016d90a81c2883f6df74
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-04-07 17:27:02 +0100

    only static files

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.locals._ = _;
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+//app.set('views', __dirname + '/views');
+//app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index');
